6b7d2afe178173f3a86a09ee32576f1e508c56ad
Manisha Pandey <37845282+Manisha53@users.noreply.github.com>
2019-07-26 11:25:29 +0530

    Create MOnk and Welcome Problem.java

---
+++ 1darray/MOnk and Welcome Problem.java
@@ -0,0 +1,27 @@
+
+import java.util.*;
+
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        int a[]=new int[n];
+        int b[]=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=in.nextInt();
+        }
+        
+        for(int i=0;i<n;i++){
+            b[i]=in.nextInt();
+        }
+        
+        for(int i=0;i<n;i++){
+            a[i]+=b[i];
+        }
+
+        for(int i=0;i<n;i++){
+        System.out.print(a[i]+" ");
+        }
+
+    }
+}
