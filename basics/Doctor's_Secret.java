de32e462008608f068d768bd857b6091d781a621
Manisha Pandey <37845282+Manisha53@users.noreply.github.com>
2019-10-15 17:23:38 +0530

    Create Doctor's_Secret.java

---
+++ basics/Doctor's_Secret.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+        Scanner in=new Scanner(System.in);
+        int l=in.nextInt();
+        int n=in.nextInt();
+        if( l<=23 && 500<n && n<1000){
+            System.out.println("Take Medicine");
+        }
+        else{
+            System.out.println("Don't take Medicine");
+        }
+
+    }
+}
