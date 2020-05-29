7e9e683ae24e17dd6613e43a63e42ab36fa7749d
Manisha Pandey <37845282+Manisha53@users.noreply.github.com>
2020-05-29 22:02:39 +0530

    Create Count_Divisors.java

---
+++ basics/Count_Divisors.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+        Scanner in=new Scanner(System.in);
+        int l=in.nextInt();
+        int r=in.nextInt();
+        int k=in.nextInt();
+        int count=0;
+        for(int i=l;i<=r;i++){
+            if(i%k==0) count++;
+        }
+        System.out.println(count);
+    }
+}
